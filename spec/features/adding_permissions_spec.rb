1f79aa0841c4b01658b798c54687832b746155b4
John Donner <johnbdonner@gmail.com>
2018-06-05 10:51:26 -0400

    [REFACTOR] grantinee -f filename

--- spec/features/adding_permissions_spec.rb
+++ spec/features/adding_permissions_spec.rb
@@ -4,34 +4,50 @@ require "spec_helper"
 require "grantinee/engine/postgresql"
 require "pg"
 
+require "support/shared_contexts/postgresql_database"
+
 RSpec.describe "Adding permissions" do
   context "when a permissions file exists with defined permissions" do
-    let(:database_name) { "grantinee_client_test" }
+    subject { `bin/grantinee -f Grantinee.test #{config}` }
+
+    let(:database) { "grantinee_test" }
     let(:service) { "my_service" }
 
     let(:permissions) do
       %(
-        Grantinee.on "#{database_name}", engine: :#{engine} do
+        on "#{database}" do
           # User on any host
           user :#{service} do
             select :users, [ :id, :anonymized ]
-            select :lists_users
+            insert :users, [ :anonymized ]
+            update :users, [ :anonymized ]
+
+            all :lists_users
           end
         end
       )
     end
 
-    before { IO.write("./permissions.rb", permissions) }
-    after { `rm ./permissions.rb` }
+    before do
+      IO.write("./Grantinee.test", permissions)
+
+      # NOTE: mock script to use test file
+      allow(Grantinee::Dsl).to receive(:eval).and_call_original
+      allow(Grantinee::Dsl).to receive(:eval).with(File.read('Grantinee')) do
+        Grantinee::Dsl.eval(File.read('Grantinee.test'))
+      end
+    end
+
+    after { `rm ./Grantinee.test` }
 
     context "when defining permissions for mysql" do
-      let(:engine) { :mysql }
+      let(:config) { "-c spec/fixtures/config_mysql.yml" }
 
       # TODO
     end
 
     context "when defining permissions for postgres" do
-      let(:engine) { :postgresql }
+      let(:config) { "-c spec/fixtures/config_postgresql.yml" }
 
       # A root engine to create/drop the database
       let(:pgsql_no_db_admin) do
@@ -43,55 +59,17 @@ RSpec.describe "Adding permissions" do
         )
       end
 
-      # A root engine that we can manipulate the database with
-      let(:pgsql_admin) do
-        PG::Connection.open(
-          user:     nil,
-          password: nil,
-          host:     Grantinee.config.dig(:postgresql, :hostname),
-          port:     Grantinee.config.dig(:postgresql, :port),
-          dbname:   database_name
-        )
-      end
-
-      # NOTE: the actual client that we assign permissions for
-      let(:postgresql_client) do
-        PG::Connection.open(
-          user:     service,
-          password: "fake_password",
-          host:     Grantinee.config.dig(:postgresql, :hostname),
-          port:     Grantinee.config.dig(:postgresql, :port),
-          dbname:   database_name
-        )
-      end
-
-      before do
-        pgsql_no_db_admin.exec "CREATE DATABASE #{database_name};"
-
-        pgsql_admin.exec "CREATE ROLE #{service} NOINHERIT LOGIN PASSWORD 'fake_password';"
-        pgsql_admin.exec "DROP TABLE IF EXISTS users;"
-        pgsql_admin.exec "CREATE TABLE users(id VARCHAR(30) PRIMARY KEY, anonymized boolean);"
-        pgsql_admin.exec "INSERT INTO users(id, anonymized) VALUES('12345e6', false);"
-      end
-
-      after do
-        postgresql_client.close
-
-        pgsql_admin.exec "DROP TABLE IF EXISTS users;"
-        pgsql_admin.exec "DROP ROLE #{service};"
-        pgsql_admin.close
-
-        pgsql_no_db_admin.exec "DROP DATABASE #{database_name};"
-        pgsql_no_db_admin.close
-      end
-
       it "grants the service the defined privileges" do
+        subject
+
         expect {
           postgresql_client.exec("SELECT id, anonymized FROM users;")
         }.not_to raise_error
       end
 
       it "denies the service any privilege that is not allowed" do
+        subject
+
         expect {
           postgresql_client.exec("SELECT * FROM users;")
         }.to raise_error(PG::InsufficientPrivilege)
---
+++ spec/support/shared_contexts/postgresql_database.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+require "support/postgresql_helpers"
+require "byebug"
+
+RSpec.shared_context "postgresql database" do
+  # NOTE: the actual client that we assign permissions for
+  let(:postgresql_client) do
+    configuration = YAML.safe_load(File.read("spec/fixtures/config_postgresql.yml"))
+
+    PG::Connection.open(
+      user:     service,
+      password: "fake_password",
+      host:     configuration["hostname"],
+      port:     configuration["port"],
+      dbname:   configuration["database"]
+    )
+  end
+
+  before do
+    pg_admin = PostgresqlHelpers::Postgresql.new
+    pg_admin.create_database(database)
+    pg_admin.create_role(service, "fake_password")
+    pg_admin.close
+
+    db_admin = PostgresqlHelpers::Postgresql.new(database: database)
+    db_admin.create_users_table
+    db_admin.create_user_records
+    db_admin.close
+  end
+
+  after do
+    postgresql_client.close
+    pg_admin = PostgresqlHelpers::Postgresql.new
+    pg_admin.drop_database(database)
+    pg_admin.close
+  end
+end
