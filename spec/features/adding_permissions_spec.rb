28e4a963de88617cbd7ec27b58b8a368f3a1eb29
John Donner <johnbdonner@gmail.com>
2018-06-05 01:20:36 -0400

    [REFACTOR] postgres spec context

--- spec/features/adding_permissions_spec.rb
+++ spec/features/adding_permissions_spec.rb
@@ -4,14 +4,18 @@ require "spec_helper"
 require "grantinee/engine/postgresql"
 require "pg"
 
+require "support/shared_contexts/postgresql_database"
+
 RSpec.describe "Adding permissions" do
   context "when a permissions file exists with defined permissions" do
+    subject { Grantinee::Dsl.eval(File.read('./Grantinee.test')) }
+
     let(:database) { "grantinee_client_test" }
     let(:service) { "my_service" }
 
     let(:permissions) do
       %(
-        Grantinee.on "#{database}", engine: :#{engine} do
+        on "#{database}", :#{engine} do
           # User on any host
           user :#{service} do
             select :users, [ :id, :anonymized ]
@@ -21,8 +25,8 @@ RSpec.describe "Adding permissions" do
       )
     end
 
-    before { IO.write("./permissions.rb", permissions) }
-    after { `rm ./permissions.rb` }
+    before { IO.write("./Grantinee.test", permissions) }
+    after { `rm ./Grantinee.test` }
 
     context "when defining permissions for mysql" do
       let(:engine) { :mysql }
@@ -33,65 +37,19 @@ RSpec.describe "Adding permissions" do
     context "when defining permissions for postgres" do
       let(:engine) { :postgresql }
 
-      # A root engine to create/drop the database
-      let(:pgsql_no_db_admin) do
-        PG::Connection.open(
-          user:     nil,
-          password: nil,
-          host:     Grantinee.config.dig(:postgresql, :hostname),
-          port:     Grantinee.config.dig(:postgresql, :port)
-        )
-      end
-
-      # A root engine that we can manipulate the database with
-      let(:pgsql_admin) do
-        PG::Connection.open(
-          user:     nil,
-          password: nil,
-          host:     Grantinee.config.dig(:postgresql, :hostname),
-          port:     Grantinee.config.dig(:postgresql, :port),
-          dbname:   database
-        )
-      end
-
-      # NOTE: the actual client that we assign permissions for
-      let(:postgresql_client) do
-        PG::Connection.open(
-          user:     service,
-          password: "fake_password",
-          host:     Grantinee.config.dig(:postgresql, :hostname),
-          port:     Grantinee.config.dig(:postgresql, :port),
-          dbname:   database
-        )
-      end
-
-      before do
-        pgsql_no_db_admin.exec "CREATE DATABASE #{database};"
-
-        pgsql_admin.exec "CREATE ROLE #{service} NOINHERIT LOGIN PASSWORD 'fake_password';"
-        pgsql_admin.exec "DROP TABLE IF EXISTS users;"
-        pgsql_admin.exec "CREATE TABLE users(id VARCHAR(30) PRIMARY KEY, anonymized boolean);"
-        pgsql_admin.exec "INSERT INTO users(id, anonymized) VALUES('12345e6', false);"
-      end
-
-      after do
-        postgresql_client.close
-
-        pgsql_admin.exec "DROP TABLE IF EXISTS users;"
-        pgsql_admin.exec "DROP ROLE #{service};"
-        pgsql_admin.close
-
-        pgsql_no_db_admin.exec "DROP DATABASE #{database};"
-        pgsql_no_db_admin.close
-      end
+      include_context "postgresql database"
 
       it "grants the service the defined privileges" do
+        # subject
+
         expect {
           postgresql_client.exec("SELECT id, anonymized FROM users;")
         }.not_to raise_error
       end
 
       it "denies the service any privilege that is not allowed" do
+        # subject
+
         expect {
           postgresql_client.exec("SELECT * FROM users;")
         }.to raise_error(PG::InsufficientPrivilege)
---
+++ spec/support/postgresql_helpers.rb
@@ -0,0 +1,53 @@
+
+module PostgresqlHelpers
+  class Postgresql
+    def initialize(user: nil, password: nil, database: nil)
+      @client = PG::Connection.open(
+        user:     user,
+        password: password,
+        host:     Grantinee.config.dig(:postgresql, :hostname),
+        port:     Grantinee.config.dig(:postgresql, :port),
+        dbname:   database
+      )
+    end
+
+    def create_database(database)
+      @client.exec "CREATE DATABASE #{database};"
+    rescue PG::DuplicateDatabase
+      puts "#{database} database already exists, ignoring..."
+    end
+
+    def drop_database(database)
+      @client.exec "DROP DATABASE #{database};"
+    end
+
+    def create_role(role, password)
+      @client.exec "CREATE ROLE #{role} NOINHERIT LOGIN PASSWORD '#{password}';"
+    rescue PG::DuplicateObject
+      puts "#{role} role already exists, ignoring..."
+    end
+
+    def drop_role(role)
+      @client.exec "DROP ROLE #{role};"
+    end
+
+    def create_users_table
+      @client.exec "CREATE TABLE users(id VARCHAR(30) PRIMARY KEY, anonymized boolean);"
+    rescue StandardError
+      drop_users_table
+      @client.exec "CREATE TABLE users(id VARCHAR(30) PRIMARY KEY, anonymized boolean);"
+    end
+
+    def drop_users_table
+      @client.exec "DROP TABLE IF EXISTS users;"
+    end
+
+    def create_user_records
+      @client.exec "INSERT INTO users(id, anonymized) VALUES('12345e6', false);"
+    end
+
+    def close
+      @client.close
+    end
+  end
+end
