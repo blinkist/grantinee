8ed5c540e4c8d90a0e0e79e0f0238ebb145f628d
John Donner <johnbdonner@gmail.com>
2018-06-07 14:50:06 -0400

    [REFACTOR] super DRY

--- spec/features/adding_permissions_spec.rb
+++ spec/features/adding_permissions_spec.rb
@@ -6,6 +6,13 @@ require "support/shared_contexts/postgresql_database"
 require "support/shared_contexts/permissions"
 require "support/query_helpers"
 
+def db_error_args
+  {
+    mysql: [Mysql2::Error, /command denied to user/],
+    postgresql: [PG::InsufficientPrivilege]
+  }
+end
+
 RSpec.describe "Adding permissions" do
   include QueryHelpers
 
@@ -16,86 +23,57 @@ RSpec.describe "Adding permissions" do
 
     include_context "permissions"
 
-    context "for mysql" do
-      let(:config) { "-c spec/fixtures/config_mysql.yml" }
-      let(:raised_error_args) { [Mysql2::Error, /command denied to user/] }
+    %i[mysql postgresql].each do |db_type|
+      context "for #{db_type}" do
+        let(:config) { "-c spec/fixtures/config_#{db_type}.yml" }
+        let(:raised_error_args) { db_error_args[db_type] }
 
-      include_context "mysql database"
+        include_context "#{db_type} database"
 
-      before { subject }
+        before { subject }
 
-      context "when the user can select all fields" do
-        let(:permissions) do
-          -> { select :users, [ :id, :anonymized ] }
+        context "when the user can select all fields" do
+          let(:permissions) do
+            -> { select :users, [ :id, :anonymized ] }
+          end
+
+          it "cannot insert records" do
+            expect { query(db_type, :insert) }.to raise_error(*raised_error_args)
+          end
+
+          it "can select records" do
+            expect { query(db_type, :select) }.not_to raise_error
+          end
+
+          it "cannot update records" do
+            expect { query(db_type, :update) }.to raise_error(*raised_error_args)
+          end
+
+          it "cannot delete records" do
+            expect { query(db_type, :delete) }.to raise_error(*raised_error_args)
+          end
         end
 
-        it "cannot insert records" do
-          expect { query(db_type, :insert) }.to raise_error(*raised_error_args)
-        end
+        context "when the user can insert records for a table" do
+          let(:permissions) do
+            -> { insert :users }
+          end
 
-        it "can select records" do
-          expect { query(db_type, :select) }.not_to raise_error
-        end
+          it "can insert records" do
+            expect { query(db_type, :insert) }.not_to raise_error
+          end
 
-        it "cannot update records" do
-          expect { query(db_type, :update) }.to raise_error(*raised_error_args)
-        end
+          it "cannot select records" do
+            expect { query(db_type, :select) }.to raise_error(*raised_error_args)
+          end
 
-        it "cannot delete records" do
-          expect { query(db_type, :delete) }.to raise_error(*raised_error_args)
-        end
-      end
-    end
+          it "cannot update records" do
+            expect { query(db_type, :update) }.to raise_error(*raised_error_args)
+          end
 
-    context "for postgres" do
-      let(:config) { "-c spec/fixtures/config_postgresql.yml" }
-      let(:raised_error_args) { [PG::InsufficientPrivilege] }
-
-      include_context "postgresql database"
-
-      before { subject }
-
-      context "when the user can select all fields" do
-        let(:permissions) do
-          -> { select :users, [ :id, :anonymized ] }
-        end
-
-        it "cannot insert records" do
-          expect { query(db_type, :insert) }.to raise_error(*raised_error_args)
-        end
-
-        it "can select records" do
-          expect { query(db_type, :select) }.not_to raise_error
-        end
-
-        it "cannot update records" do
-          expect { query(db_type, :update) }.to raise_error(*raised_error_args)
-        end
-
-        it "cannot delete records" do
-          expect { query(db_type, :delete) }.to raise_error(*raised_error_args)
-        end
-      end
-
-      context "when the user can insert records for a table" do
-        let(:permissions) do
-          -> { insert :users }
-        end
-
-        it "can insert records" do
-          expect { query(db_type, :insert) }.not_to raise_error
-        end
-
-        it "cannot select records" do
-          expect { query(db_type, :select) }.to raise_error(*raised_error_args)
-        end
-
-        it "cannot update records" do
-          expect { query(db_type, :update) }.to raise_error(*raised_error_args)
-        end
-
-        it "cannot delete records" do
-          expect { query(db_type, :delete) }.to raise_error(*raised_error_args)
+          it "cannot delete records" do
+            expect { query(db_type, :delete) }.to raise_error(*raised_error_args)
+          end
         end
       end
     end
