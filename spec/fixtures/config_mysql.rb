d7e3fe83a739fe30d8e0997f80cb48ede6a8b04e
John Donner <johnbdonner@gmail.com>
2018-06-11 16:22:49 +0200

    [REFACTOR] return of the localhost

--- spec/fixtures/config_mysql.rb
+++ spec/fixtures/config_mysql.rb
@@ -2,7 +2,7 @@ Grantinee.configure do |c|
   c.engine = 'mysql'
   c.username = 'root'
   c.password = 'mysql'
-  c.hostname = 'mysql'
+  c.hostname = '127.0.0.1'
   c.port     = 3306
   c.database = 'grantinee_test'
 end
