ce8278eaf6b1f0fedecd3df03f5aabf15f7ed1d4
Paweł Komarnicki <pawel@blinkist.com>
2018-06-12 15:02:18 +0200

    Improvements to Grantinee module

--- lib/grantinee.rb
+++ lib/grantinee.rb
@@ -1,10 +1,11 @@
 # frozen_string_literal: true
+
 require 'logger'
 
 # Grantinee module is where the magic at ;-)
 module Grantinee
   class << self
-    attr_reader :logger
+    attr_accessor :logger
 
     # Allow configuration using a block
     def configure
@@ -15,17 +16,13 @@ module Grantinee
     def configuration
       @configuration ||= Configuration.new
     end
-
-    def logger=(logger)
-      @logger = logger
-    end
   end
 end
 
 # Load internal stuffs
 require 'grantinee/configuration'
+require 'grantinee/engine'
 require 'grantinee/cli'
 require 'grantinee/dsl'
 require 'grantinee/executor'
-require 'grantinee/engine'
 require 'grantinee/engine/abstract_engine'
